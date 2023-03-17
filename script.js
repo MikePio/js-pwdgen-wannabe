ccfcba74a6d45ace2264f5bd7420b87177a48a5f
MikePio <michelepiopilla@gmail.com>
2023-03-17 15:17:00 +0100

    started exercise + all steps to do in js

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+In console:
+-creare un variabile con un prompt che chiede il nome
+-creare un variabile con un prompt che chiede il cognome
+-creare un variabile con un prompt che chiede il colore preferito
+-creare una variabile contenente il numero 23
+-unire tutte le stringhe e il numero 23 per generare un password fake
+
+*facoltativo
+In pagina:
+-mostra tutti i dati inseriti e generati
+*/
+
+console.log('test');
